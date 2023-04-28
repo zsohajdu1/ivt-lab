a61f1a792a87612085119b59e78ffad9ccf6409a
zsohajdu1 <zsohajdu1@gmail.com>
2023-04-28 12:58:27 +0200

    fix all fire #2

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,8 +80,8 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
-        primaryTorpedoStore.fire(1);
-        secondaryTorpedoStore.fire(1);
+        firingSuccess = primaryTorpedoStore.fire(1);
+        firingSuccess = secondaryTorpedoStore.fire(1) || firingSuccess;
         wasPrimaryFiredLast = false;
 
         break;
